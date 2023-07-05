18bbd509736730a386335a5fc3b6f8b45861f322
VisakanTheGamer <83350702+VisakanTheGamer@users.noreply.github.com>
2023-07-05 21:30:23 +0800

    Completely remade the Player,the movement script and mouselook script
    the mouse look bug is yet to be fixed

--- NeverEnding/Assets/Scripts/MouseLook.cs
+++ NeverEnding/Assets/Scripts/MouseLook.cs
@@ -4,41 +4,33 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float sensX;
-    public float sensY;
-    Camera cam;
+public Transform player;
 
-    public Transform orientation;
-    public Transform playerBody;
-
-    float xRotation;
-    float yRotation;
-
-    private void Start()
+    void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        // Update camera position to match player position
+        transform.position = player.position;
+
+        // Update player rotation to match camera rotation
+        player.rotation = transform.rotation;
     }
-
-    private void Update()
-    {
-        //get mouse input
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
-
-        playerBody.Rotate(Vector3.up * mouseX);
-
-        yRotation += mouseX;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
-    }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
